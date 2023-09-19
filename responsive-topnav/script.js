78936dcdac3ce1d6a0b9f88d7e5d2ca5d36372fe
colecrum26 <cwcrumrine@gmail.com>
2023-09-19 16:09:28 -0500

    don't forget to link scripts and stylesheetsgit add .

--- responsive-topnav/script.js
+++ responsive-topnav/script.js
@@ -1,4 +1,8 @@
 function myFunction() {
-    const nav = document.getElementById('myTopnav');
-    return nav.className === 'topnav' ? nav.className += 'responsive' : nav.className = 'topnav';
+    const nav = document.getElementById("myTopnav");
+    if (nav.className === "topnav") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "topnav";
+    }
 }
